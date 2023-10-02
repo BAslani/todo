7cfe42862e7c293a96b35283b96b7fd23590e8cb
Behzad Aslani <behzadaslani80@gmail.com>
2023-10-02 13:46:11 +0330

    Build: Login page setup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,66 @@
 import React from 'react'
+import styled from 'styled-components'
+
 
 const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div>Login</div>
+    <Wrapper>
+      <header>
+        <div className="container">
+          <svg xmlns="http://www.w3.org/2000/svg" width="84" height="97" viewBox="0 0 84 97" fill="none" className='check'>
+            <path d="M53.2174 0H30.6038C25.5999 0 21.5103 4.04155 21.5103 9.04538V13.5681C21.5103 18.5719 25.5518 22.6135 30.5556 22.6135H53.2174C58.2212 22.6135 62.2627 18.5719 62.2627 13.5681V9.04538C62.3109 4.04155 58.2212 0 53.2174 0Z" fill="white" />
+            <path d="M67.1189 13.5656C67.1189 21.2157 60.8641 27.4705 53.214 27.4705H30.6003C22.9503 27.4705 16.6955 21.2157 16.6955 13.5656C16.6955 10.8713 13.8086 9.18727 11.403 10.4382C4.61892 14.0468 0 21.2157 0 29.4432V74.7182C0 86.5542 9.67086 96.2251 21.5068 96.2251H62.3075C74.1435 96.2251 83.8143 86.5542 83.8143 74.7182V29.4432C83.8143 21.2157 79.1954 14.0468 72.4114 10.4382C70.0057 9.18727 67.1189 10.8713 67.1189 13.5656ZM57.9772 51.6236L38.7318 70.8691C38.01 71.5908 37.0959 71.9276 36.1817 71.9276C35.2676 71.9276 34.3534 71.5908 33.6317 70.8691L26.4144 63.6521C25.0191 62.2568 25.0191 59.9473 26.4144 58.552C27.8097 57.1567 30.1192 57.1567 31.5145 58.552L36.1817 63.2191L52.8772 46.5236C54.2725 45.1283 56.582 45.1283 57.9772 46.5236C59.3725 47.9189 59.3725 50.2283 57.9772 51.6236Z" fill="white" />
+          </svg>
+        </div>
+        <h4 className='title'>Let's get started!</h4>
+      </header>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text" placeholder='Username' className="form-control" />
+        </div>
+        <div>
+          <input type="password" placeholder='password' className="form-control" />
+        </div>
+        <button className="btn" type='submit'>sign up / Login</button>
+      </form>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.main`
+.container {
+  background: #3e9fbd;
+  width: 10rem;
+  margin: 5rem auto;
+  margin-bottom: 1rem;
+  border-radius: 0.75rem;
+  text-align: center;
+}
+.check {
+  font-size: 7rem;
+  margin: 1.5rem auto;
+  color: #fff;
+}
+form {
+  text-align: center;
+  margin-top: 3rem;
+}
+.form-control {
+  width: 16rem;
+  background: #F0F0F0;
+  padding: 0.75rem;
+  border: 1px solid #747474;
+  border-radius: 0.75rem;
+  font-size: 1rem;
+  margin-bottom: 1.5rem;
+}
+.btn {
+  border-radius: 0.75rem;
+  padding: 0.75rem 2rem;
+}
+`
 
 export default Login
