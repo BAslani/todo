cfeb7f0f7b0955bc4f90c08a7c724dae2058a6a0
Behzad Aslani <behzadaslani80@gmail.com>
2023-10-03 14:51:39 +0330

    Build: Home page setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+import { BsPersonCircle } from 'react-icons/bs'
+import { FaBars } from 'react-icons/fa6'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Wrapper>
+      <div className="user">
+        <BsPersonCircle />
+        <h4>username</h4>
+      </div>
+      <button className="toggle-btn">
+        <FaBars />
+      </button>
+    </Wrapper>
   )
 }
+
+
+const Wrapper = styled.nav`
+
+`
 
 export default Navbar
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import Task from '../components/Task'
+import styled from 'styled-components'
+import { useGlobal } from '../context/context'
+import { BsFillPlusCircleFill, BsSortDownAlt, BsCalendar3 } from 'react-icons/bs'
 
 const Home = () => {
+  const { tasks } = useGlobal()
   return (
-    <div>Home</div>
+    <Wrapper>
+      <Navbar />
+      {
+        tasks.map((task) => {
+          return <Task key={task.id} {...task} />
+        })
+      }
+      <div className="btn-container">
+        <button className="add-btn" type='button'>
+          <BsFillPlusCircleFill />
+        </button>
+        <button type="button" className="sort-btn">
+          <BsSortDownAlt />
+        </button>
+      </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.main`
+
+`
 
 export default Home
