8d2e023f4406df0b0bb62a7b04114d24bd38b358
Behzad Aslani <behzadaslani80@gmail.com>
2023-10-05 13:41:56 +0330

    Feat(Home): Added add button and sort button to UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,34 +3,62 @@ import Navbar from '../components/Navbar'
 import Task from '../components/Task'
 import styled from 'styled-components'
 import { useGlobal } from '../context/context'
-import { BsFillPlusCircleFill, BsSortDownAlt, BsCalendar3 } from 'react-icons/bs'
+import { BsFillPlusCircleFill, BsSortDownAlt } from 'react-icons/bs'
 
 const Home = () => {
   const { tasks } = useGlobal()
   return (
     <Wrapper>
       <Navbar />
-      <main className='tasks-container'>
+      <section className='tasks-container'>
         {
           tasks.map((task) => {
             return <Task key={task.id} {...task} />
           })
         }
-        <div className="btn-container">
-          <button className="add-btn" type='button'>
-            <BsFillPlusCircleFill />
-          </button>
-          <button type="button" className="sort-btn">
-            <BsSortDownAlt />
-          </button>
-        </div>
-      </main>
+      </section>
+      <section className="footer">
+        <button className="add-btn" type='button'>
+          <BsFillPlusCircleFill />
+        </button>
+        <button type="button" className="sort-btn">
+          <BsSortDownAlt />
+        </button>
+      </section>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.main`
+height: 100vh;
+display: grid;
+grid-template-rows: auto 1fr auto;
+overflow-x: scroll;
 
+.tasks-container {
+  background: #eee;
+}
+.footer {
+  height: 9rem;
+  background: #fff;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
+  display: flex;
+  justify-content: space-around;
+}
+.add-btn {
+  background: transparent;
+  border: none;
+  font-size: 5rem;
+  color: #3E9FBD;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+}
+.sort-btn {
+  background: transparent;
+  border: none;
+  font-size: 5rem;
+  color: #747474;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+}
 `
 
 export default Home
