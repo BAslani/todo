138c871c88a9c08d5349fd04ec7131c40b22a19f
Behzad Aslani <behzadaslani80@gmail.com>
2023-10-10 21:44:03 +0330

    Feat: Added logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,28 @@ import styled from 'styled-components'
 import { BsPersonCircle } from 'react-icons/bs'
 import { TbLogout } from 'react-icons/tb'
 import { useGlobal } from '../context/context'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
-  const {user} = useGlobal()
+  const navigate = useNavigate()
+  const {user, setUser} = useGlobal()
   return (
     <Wrapper>
       <div className="user">
         <BsPersonCircle className='avatar' />
         <h5>{user.username}</h5>
       </div>
-      <button className="logout-btn">
+      <button
+      className="logout-btn"
+      onClick={() => {
+        setUser({
+          id: null,
+          username: '',
+          tasks: []
+        });
+        navigate('login')
+      }}
+      >
         <TbLogout />
       </button>
     </Wrapper>
