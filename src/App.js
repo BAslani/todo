cd7b2c363187e31fb7ae69efd2e30621ebef1ba7
Behzad Aslani <behzadaslani80@gmail.com>
2023-12-08 19:01:31 +0330

    test: deleted login

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Error from './pages/Error'
-import Sidebar from './components/Sidebar'
-import Register from './pages/Register'
-import PrivateRoute from './pages/PrivateRoute'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Error from "./pages/Error";
+import Sidebar from "./components/Sidebar";
+import Register from "./pages/Register";
+import PrivateRoute from "./pages/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Sidebar />
       <Routes>
-        <Route path='/' element={
-          <PrivateRoute>
-            <Home />
-          </PrivateRoute>
-        } />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='*' element={<Error />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
